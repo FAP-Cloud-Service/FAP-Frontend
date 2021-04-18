342677d3dcbcc49b9438516a7455fdc401a7efd3
Timon Wansing <timon.wansing@d-velop.de>
2021-04-18 14:19:40 +0200

    Added start-page with link to login

--- FriendsAndPlaces/src/app/start-page/start-page.component.ts
+++ FriendsAndPlaces/src/app/start-page/start-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-start-page',
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class StartPageComponent {
 
+  @Output() selectedPage = new EventEmitter<string>();
+
   constructor() { }
+
+  goToLoginPage() {
+    this.selectedPage.emit('login');
+  }
 
 }
