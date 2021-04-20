3f7790b5147c57b1b6e10a0ba3ad9b1b17957907
Leon Schönhoff <leon.schoenhoff@gmail.com>
2021-04-20 22:38:58 +0200

    added basic validation to register form

--- FriendsAndPlaces/src/app/register/register.component.ts
+++ FriendsAndPlaces/src/app/register/register.component.ts
@@ -9,28 +9,38 @@ import {MatDialogRef} from '@angular/material/dialog';
 })
 export class RegisterComponent implements OnInit {
   loading = false;
+  heading = 'Neuen Account anlegen...';
   personalForm = new FormGroup({
     firstName: new FormControl('', Validators.required),
     lastName: new FormControl('', Validators.required)
   });
   addressForm = new FormGroup({
-    street: new FormControl(),
-    zip: new FormControl(),
-    city: new FormControl(),
-    country: new FormControl()
+    street: new FormControl('', Validators.required),
+    zip: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    country: new FormControl('', Validators.required)
   });
   contactForm = new FormGroup({
-    mobile: new FormControl(),
-    email: new FormControl(),
+    mobile: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
   accountForm = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl(),
-    passwordConfirm: new FormControl()
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    passwordConfirm: new FormControl('', Validators.required),
+    termsOfService: new FormControl('', Validators.requiredTrue)
   });
   constructor(public dialogRef: MatDialogRef<RegisterComponent>) { }
   closeDialog(): void {
     this.dialogRef.close();
+  }
+  submit(): void {
+    this.heading = 'Account wird erstellt. Bitte warten...';
+    this.loading = true;
+    this.addressForm.disable();
+    this.accountForm.disable();
+    this.contactForm.disable();
+    this.personalForm.disable();
   }
   ngOnInit(): void {
   }
