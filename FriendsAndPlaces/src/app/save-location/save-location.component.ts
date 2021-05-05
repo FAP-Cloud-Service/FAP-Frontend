e534900f2095eab69261c40293a27a2e0b64d9da
Leon Schönhoff <leon.schoenhoff@gmail.com>
2021-05-05 12:42:58 +0200

    added automatic location update

--- FriendsAndPlaces/src/app/save-location/save-location.component.ts
+++ FriendsAndPlaces/src/app/save-location/save-location.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
-import {Observable} from "rxjs";
-import {LocationService} from "../services/location.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+import {LocationService} from '../services/location.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-save-location',
@@ -10,34 +11,54 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./save-location.component.scss']
 })
 export class SaveLocationComponent implements OnInit {
-  latitude: number;
-  longitude: number;
-  constructor(public dialogRef: MatDialogRef<any>, private locationService: LocationService, private snackBar: MatSnackBar) { }
+  latitude: any;
+  longitude: any;
+  loading = false;
+  positionForm: FormGroup = new FormGroup({
+    latitude: new FormControl('', Validators.required),
+    longitude: new FormControl('', Validators.required)
+  });
+
+  constructor(public dialogRef: MatDialogRef<any>, private locationService: LocationService, private snackBar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
+    this.getGeoPosition();
   }
+
   closeDialog(): void {
     this.dialogRef.close();
   }
-  getUserLocation(): void {
-    console.log('Frage Standortdaten ab');
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position: GeolocationPosition) => {
-          console.log('Standortdaten wurden abgefragt');
-        }
-      );
-    }
-    else {
-      this.snackBar.open('Standortabfrage ist auf diesem Gerät nicht verfügbar');
-    }
+
+  getGeoPosition(): void {
+    navigator.geolocation.getCurrentPosition((position => {
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+      this.positionForm.controls.latitude.setValue(this.latitude);
+      this.positionForm.controls.longitude.setValue(this.longitude);
+      this.positionForm.disable();
+    }));
   }
+
   submitLocation(): any {
-    if (this.latitude && this.longitude) {
-      this.locationService.submitLocation(this.latitude, this.longitude);
+    this.loading = true;
+    const positionFormControls = this.positionForm.controls;
+    this.locationService.submitLocation(positionFormControls.latitude.value, positionFormControls.longitude.value).subscribe(
+      () => {
+        this.dialogRef.close();
+      },
+      (error: HttpErrorResponse) => {
+        this.snackBar.open('Ein Fehler ist aufgetreten: ' + error.status, '', {duration: 6000});
+        this.loading = false;
+      }
+    );
+  }
+  changeFormState(): void {
+    if (this.positionForm.enabled) {
+      this.positionForm.disable();
     }
     else {
-      this.snackBar.open('Dein Standort ist nicht verfügbar', '', { duration: 5000 });
+      this.positionForm.enable();
     }
   }
 }
