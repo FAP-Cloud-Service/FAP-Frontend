08ea8d87f7189da0a20ed80a2cabb490de8b897a
Leon Schönhoff <leon.schoenhoff@gmail.com>
2021-05-12 13:00:24 +0200

    responsive design on homepage

--- FriendsAndPlaces/src/app/start-page/start-page.component.ts
+++ FriendsAndPlaces/src/app/start-page/start-page.component.ts
@@ -8,13 +8,25 @@ import {MatDialog} from '@angular/material/dialog';
   templateUrl: './start-page.component.html',
   styleUrls: ['./start-page.component.scss']
 })
-export class StartPageComponent{
+export class StartPageComponent implements OnInit{
   registerDialogOpen = false;
+  numberOfColumns: number;
+  breakPoint: boolean;
   @Output() selectedPage = new EventEmitter<string>();
 
   @Input() loggedIn: boolean;
-
   constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.numberOfColumns = (window.innerWidth <= 1100) ? 1 : 3;
+    this.breakPoint = (window.innerWidth <= 800);
+  }
+
+  onResize(event: any): void {
+    this.numberOfColumns = (event.target.innerWidth <= 1100) ? 1 : 3;
+    this.breakPoint = (event.target.innerWidth <= 800);
+  }
+
   goToLoginPage(): void {
     this.selectedPage.emit('login');
   }
